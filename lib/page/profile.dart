93eceaaad008ed140547b5c2c49f7f1dff2bed5c
Milan Vadher <milanvadher1996@gmail.com>
2018-09-27 15:59:13 +0530

    profile page display data with design

--- lib/page/profile.dart
+++ lib/page/profile.dart
@@ -33,26 +33,66 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: SafeArea(
-        child: new Row(
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                child: new Text(_userName.toString()[0].toUpperCase())),
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            margin: EdgeInsets.all(10.0),
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: CircleAvatar(
+                    child: Text(_userName.toString()[0].toUpperCase()),
+                  ),
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      _userName,
+                      textScaleFactor: 1.2,
+                    ),
+                    Text(_contactNumber)
+                  ],
+                )
+              ],
             ),
-            Center(
-              child: Center(
-                child: new Text(_userName, style: Theme.of(context).textTheme.subhead),
+          ),
+          Card(
+            margin: EdgeInsets.all(10.0),
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Text('Question solved: '),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                    child: Text((_questionState-1).toString(), textScaleFactor: 1.2,),
+                  ),
+                ],
               ),
             ),
-            // Column(
-            //   children: <Widget>[
-                
-            //     new Text(_contactNumber),
-            //   ],
-            // ),
-          ],
-        ),
+          ),
+          Card(
+            margin: EdgeInsets.all(10.0),
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Text('Total Points: '),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 0.0),
+                    child: Text((_points).toString(), textScaleFactor: 1.2,),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
